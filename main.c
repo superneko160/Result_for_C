0a918fa142241f4776966ec819aff3b171719554
superneko <supernekocat31@gmail.com>
2023-10-29 00:00:39 +0900

    Done

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Result型のエラーを表す列挙型
+typedef enum {
+    OK,         // 成功
+    ERR_INVALID, // 無効な値
+    ERR_OVERFLOW // オーバーフロー
+} ResultCode;
+
+// Result型構造体
+typedef struct {
+    ResultCode code; // 結果のコード
+    int value;       // 結果の値 (成功時のみ)
+} Result;
+
+// Result型のファクトリ関数（成功時）
+Result Result_OK(int value) {
+    Result result;
+    result.code = OK;
+    result.value = value;
+    return result;
+}
+
+// Result型のファクトリ関数（エラー時）
+Result Result_ERR(ResultCode code) {
+    Result result;
+    result.code = code;
+    return result;
+}
+
+// プリント関数
+void printResult(Result result) {
+    switch (result.code) {
+        case OK:
+            printf("Result: OK, Value: %d\n", result.value);
+            break;
+        case ERR_INVALID:
+            printf("Result: ERR_INVALID\n");
+            break;
+        case ERR_OVERFLOW:
+            printf("Result: ERR_OVERFLOW\n");
+            break;
+        default:
+            printf("Result: Unknown\n");
+            break;
+    }
+}
+
+// メイン関数
+int main() {
+    Result result1 = Result_OK(32);     // 成功のResult
+    Result result2 = Result_ERR(ERR_INVALID); // エラーのResult
+
+    printResult(result1);
+    printResult(result2);
+
+    return 0;
+}
